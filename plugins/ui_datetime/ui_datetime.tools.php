a85541a84c9a314dd4ea7b4b24719c04b1bc0cfa
Andrey Matsovkin <macik.spb@gmail.com>
2012-09-26 23:39:16 +0400

    Deleting unnecessary code

--- plugins/ui_datetime/ui_datetime.tools.php
+++ plugins/ui_datetime/ui_datetime.tools.php
@@ -26,58 +26,11 @@ $adminhelp = $adminhelp1;
 
 require_once cot_incfile('forms');
 
-function cot_selectbox_time($utime, $name = '', $usertimezone = true, $custom_rc = '')
-{
-	global $L, $R, $usr;
-	$rc_name = preg_match('#^(\w+)\[(.*?)\]$#', $name, $mt) ? $mt[1] : $name;
-
-	$utime = ($usertimezone && $utime > 0) ? ($utime + $usr['timezone'] * 3600) : $utime;
-
-	if ($utime == 0)
-	{
-		list($s_hour, $s_minute) = array(null, null);
-	}
-	else
-	{
-		list($s_hour, $s_minute) = explode('-', @date('H-i', $utime));
-	}
-
-	$range = array();
-	for ($i = 0; $i < 24; $i++)
-	{
-		$range[] = sprintf('%02d', $i);
-	}
-	$hour = cot_selectbox($s_hour, $name.'[hour]', $range);
-
-	$range = array();
-	for ($i = 0; $i < 60; $i++)
-	{
-		$range[] = sprintf('%02d', $i);
-	}
-
-	$minute = cot_selectbox($s_minute, $name.'[minute]', $range);
-
-	$rc = empty($R["input_date_{$mode}"]) ? 'input_date' : "input_date_{$mode}";
-	$rc = empty($R["input_date_{$rc_name}"]) ? $rc : "input_date_{$rc_name}";
-	$rc = empty($custom_rc) ? $rc : $custom_rc;
-
-	$result = cot_rc($rc, array(
-					'hour' => $hour,
-					'minute' => $minute
-	));
-
-	return $result;
-}
-
-
-
 $tt = new XTemplate(cot_tplfile($plug_name.'.tools', 'plug'));
 $std_date = cot_selectbox_date(time(),'short','testdate');
 $std_time = cot_selectbox_date(time(),'time','testtime');
 
 $tt->parse();
 $plugin_body .= $tt->text();
-
-// addition code goes here....
 
 ?>
